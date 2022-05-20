7de0590e9107866779018c609335a627827936e8
halal-beef <78730004+halal-beef@users.noreply.github.com>
2022-05-20 19:58:03 +0100

    Update omni_CASPER_VIA_M1.mk

--- omni_CASPER_VIA_M1.mk
+++ omni_CASPER_VIA_M1.mk
@@ -2,9 +2,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 LOCAL_PATH := device/Casper/CASPER_VIA_M1
 
 # overlays
